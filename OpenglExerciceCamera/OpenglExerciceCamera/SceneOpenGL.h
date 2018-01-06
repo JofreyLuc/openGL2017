59396fcf4e815407dfa7c1c3146c3046815dbbb3
FGarland <antoine.delemazure@gmail.com>
2018-01-06 22:39:34 +0100

    Solution exo 6, j'espère que ça marche.

---
+++ OpenglExerciceCamera/OpenglExerciceCamera/SceneOpenGL.h
@@ -0,0 +1,65 @@
+#ifndef DEF_SCENEOPENGL
+#define DEF_SCENEOPENGL
+
+
+// Includes
+
+#ifdef WIN32
+#include <GL/glew.h>
+
+
+#else
+#define GL3_PROTOTYPES 1
+#include <GL3/gl3.h>
+
+#endif
+
+
+// Includes GLM
+
+#include <glm.hpp>
+#include <gtx/transform.hpp>
+#include <gtc/type_ptr.hpp>
+
+
+// Autres includes
+
+#include <SDL.h>
+#include <iostream>
+#include <string>
+#include <math.h>
+#include "Shader.h"
+#include "Caisse.h"
+#include "Input.h"
+#include "Texture.h"
+#include "Camera.h"
+
+
+
+// Classe
+
+class SceneOpenGL
+{
+    public:
+
+    SceneOpenGL(std::string titreFenetre, int largeurFenetre, int hauteurFenetre);
+    ~SceneOpenGL();
+
+    bool initialiserFenetre();
+    bool initGL();
+    void bouclePrincipale();
+
+
+    private:
+
+    std::string m_titreFenetre;
+    int m_largeurFenetre;
+    int m_hauteurFenetre;
+
+    SDL_Window* m_fenetre;
+    SDL_GLContext m_contexteOpenGL;
+    Input m_input;
+};
+
+#endif
+
