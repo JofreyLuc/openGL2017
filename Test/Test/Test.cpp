b4270c4e057a485a4f34d7ac0d5243ae66c9792e
Jofrey Luc <jyeil9@laposte.net>
2018-01-07 14:50:50 +0100

    ajout du projet de test

---
+++ Test/Test/Test.cpp
@@ -0,0 +1,11 @@
+// Test du setup
+
+#include <SDL.h>
+#include <GL/glew.h>
+#include <glm.hpp>
+#include <gtx/transform.hpp>
+#include <SDL_image.h>
+
+int main(int argc, char* argv[]){
+	return 0;
+}
